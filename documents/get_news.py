1a8eb82c542694f82806029ee8a6793c36a0c32d
Maria Theresia Tyas <theresia.valerie@gmail.com>
2019-03-20 18:18:58 -0500

    test get_news.py

--- documents/get_news.py
+++ documents/get_news.py
@@ -1,15 +1,19 @@
 import newspaper
 import json
 
-urls = {'abc': 'https://abcnews.go.com/', 'huffington': 'https://www.huffingtonpost.com/'}
+urls = {'yahoo_trump': 'https://news.search.yahoo.com/search?p=trump&fr=uh3_news_vert_gs&fr2=p%3Anews%2Cm%3Asb'}
+print('grace')
 
 for name, url in urls.items():
+	print('hi')
 	cnn_paper = newspaper.build(url, memoize_articles=False)
-
+	print('hi2')
 	all_articles = {}
-
+	print('hi3')
 	i = cnn_paper.size()
 	counter = 0
+
+	print('Start')
 
 	for article in cnn_paper.articles:
 		try:
